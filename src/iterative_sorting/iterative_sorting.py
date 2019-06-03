2f7070813b0bc49368949d64ba85aa34c5ffe7dc
shawn <shawnantonucci2018@gmail.com>
2019-06-03 15:32:57 -0700

    added bubble sort and passed tests

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -27,35 +27,42 @@ def selection_sort( arr ):
 
 
 array_test = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-selection_sort(array_test)
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+# selection_sort(array_test)
 
+# 1. Loop through your array
+#     - Compare each element to its neighbor
+#     - If elements in wrong position (relative to each other, swap them)
+# 2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
+
+# TO-DO:  implement the Bubble Sort function below
+
+# def swap(arr, a, b):
+#     temp = arr[a]
+#     arr[a] = arr[b]
+#     arr[b] = temp
+
+def bubble_sort(arr):
+    complete = False
+    while not complete:
+        swapped = False
+        for i in range(1, len(arr)):
+            if arr[i-1] > arr[i]:
+                temp = arr[i]
+                arr[i] = arr[i -1]
+                arr[i - 1] = temp
+                swapped = True
+        if not swapped:
+            complete = True
     return arr
 
+print(bubble_sort(array_test))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
     return arr
 
-
-#     class Book:
-# 	def __init__(self, t, a, g)
-# 		self.title = t
-# 		self.author = a
-# 		self.genre = g
-
-# def insertion_sort(books):
-# 	for i in range(1, len(books)):
-# 		temp = books[i]
-# 		j = i
-
-# 		while j > 0 and temp.genre < books[j - 1].genre:
-# 			books[j] = books[j - 1] # scoot books over to make room
-# 			j -= 1
-
-# 		# j is now the index of where we want to place the book
-# 		books[j] = temp
-
-# 	return books
+# loop though array
+# check siblings
+# if index is greater than next sibling
+# swap
