143cbc40d5ca1731c33ac8eed45ef811fda81f0a
shawn <shawnantonucci2018@gmail.com>
2019-06-04 15:23:31 -0700

    added merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -25,6 +25,11 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) > 1:
+        left = merge_sort(arr[0:len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left,right)
+        print(arr)
 
     return arr
 
