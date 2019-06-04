789bad3625b8cb0b40adf85480bafc2a6755abea
shawn <shawnantonucci2018@gmail.com>
2019-06-04 15:12:00 -0700

    added in merge

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,11 +1,26 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
+testArr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
-    return merged_arr
+    indexA = 0
+    indexB = 0
+    for i in range(0, elements):
+        if indexA >= len(arrA):
+            merged_arr[i] = arrB[indexB]
+            indexB += 1
+        elif indexB >= len(arrB):
+            merged_arr[i] = arrA[indexA]
+            indexA += 1
+        elif arrA[indexA] < arrB[indexB]:
+            merged_arr[i] = arrA[indexA]
+            indexA += 1
+        else:
+            merged_arr[i] = arrB[indexB]
+            indexB += 1
 
+    return merged_arr
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
@@ -20,7 +35,7 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
